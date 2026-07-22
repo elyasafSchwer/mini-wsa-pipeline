7e3c240b19f2dfc4e0f19ffabe29bf5e6d20c896
Elyasaf Schwer <elyasafschwer@gmail.com>
2026-07-22 16:36:41 +0300

    Initial commit: Spring Boot 3 and Java 21 base setup

---
+++ src/test/java/com/es/wsa/MiniWsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.wsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniWsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
